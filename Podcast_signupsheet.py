51263285b23f5580539e5058e278ebab3fcbe019
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-08 00:33:02 -0500

    Update Podcast_signupsheet.py

--- Podcast_signupsheet.py
+++ Podcast_signupsheet.py
@@ -1,10 +1,15 @@
-# streamlit_app.py
-
 import streamlit as st
+from google.oauth2 import service_account
 from gsheetsdb import connect
 
 # Create a connection object.
-conn = connect()
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
@@ -14,7 +19,7 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-sheet_url = st.secrets["public_gsheets_url"]
+sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
